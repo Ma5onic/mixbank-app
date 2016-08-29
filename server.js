86a092cd8022ca946d28fc72aa0b5a7a2a5f869d
Madeleine <email": null>
2016-08-29 18:18:27 +1200

    initial API setup working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const db = require ('./data/db')
 
 const app = express()
 
@@ -7,9 +8,17 @@ const PORT = 9876
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/api/v1/accounts/:id/transactions', function(req, res) {
+  db.getTransactionsByAccountId(req.params.id)
+  .then(function (data) {
+    res.send(data)
+  })
+})
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
 
 app.listen(PORT, function() {
   console.log('server is running and listening on port', PORT)
