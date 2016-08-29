0e0a53ff158101df1f86f9bdf1c8d60ed70ad5cc
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-29 16:04:23 +1200

    move listen to bottom of serverfile

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express()
 
 const PORT = 9876
 
-app.listen(PORT, function() {
-  console.log('server is running and listening on port', PORT)
-})
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, function() {
+  console.log('server is running and listening on port', PORT)
 })
