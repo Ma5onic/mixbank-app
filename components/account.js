58b9a69d408229f6a043bbf25aaf7418deff7093
Madeleine <email": null>
2016-08-29 16:50:56 +1200

    Wrote balance calculator, passes test when it should

--- components/account.js
+++ components/account.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
 
-function Account (props) {
-  return (
-    <div>
-      <h1>Welcome to the Account bit of the app</h1>
-        <ul>{props.transactions.map(function (transaction, index) {
-        return (
-        <p key={index}>
-        {transaction.description}  ${transaction.amount}
-        </p>
-      )
-    }
-  )}</ul>
+class Account extends React.Component {
 
-    </div>
-  )
+  calculateBalance(transactionsArray) {
+  return transactionsArray.map( transaction => transaction.amount )
+                            .reduce( (prev, curr) => prev + curr )
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Account bit of the app</h1>
+          <ul>{this.props.transactions.map(function (transaction, index) {
+          return (
+          <p key={index}>
+          {transaction.description}  ${transaction.amount/100}
+          </p>
+            )
+          }
+        )}</ul>
+          <h2>$ balance goes here...</h2>
+
+      </div>
+    )
+  }
+
 }
 
 export default Account
--- test/index.js
+++ test/index.js
@@ -1,6 +1,20 @@
 var test = require('tape')
+import Account from '../components/account'
 
-test('testing connect-fun', function (t) {
+test('testing mix bank app', function (t) {
   t.ok(true)
+  t.end()
+})
+
+//arrange that the expectedBalance will be +56700-456
+//based on two transations in initialState
+const expectedBalance = 56244
+
+const testArray = [ {amount: -456}, {amount: 56700} ]
+
+test('calculateBalance uses transactions to calculate balance', function (t) {
+  const testAccount = new Account
+  const actualBalance = testAccount.calculateBalance(testArray)
+  t.equal(expectedBalance, actualBalance)
   t.end()
 })
