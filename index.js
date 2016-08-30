2bcf86dd8822c9aed16067afa75fae2230bbff82
Madeleine <email": null>
2016-08-30 19:13:27 +1200

    remove transactions from initial state

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ const initialState = {
   account:
     {
       id: 1,
-      transactions:[
-        {
-          id:503,
-          amount: -666,
-          description: 'purchase bk',
-          from_account_id: 1,
-          to_account_id: 2
-        },
-        {
-          id:504,
-          amount: 56700,
-          description: 'weekly pay from bk',
-          from_account_id: 2,
-          to_account_id: 1
-        }
-      ]
+      transactions:[]
     }
 }
 
