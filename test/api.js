6029ffdd0ea21c9cfa673791efc2c50b71fae444
Madeleine <email": null>
2016-08-30 15:30:12 +1200

    tests for api working but redundant to other test branch

--- test/api.js
+++ test/api.js
@@ -3,11 +3,21 @@ var request = require('supertest')
 var app = require('../app')
 
 request(app)
-  .get('/api/v1/account/2/transactions')
+  .get('/api/v1/accounts/1/transactions')
   .expect('Content-Type', /json/)
   .expect(200)
   .end(function(err, res) {
     var data = res.body
+    test('api delivers the right data', (t) => {
+      t.error(err, 'there should be no error')
 
+      t.true(data.account, 'there is a top level "account" key')
 
-test('api delivers the right')
+      t.true(Array.isArray(data.account.transactions), 'a "transactions" array is delivered')
+
+      t.equal(Number(data.account.id), 1, 'account id is the same as url')
+
+      t.end()
+      process.exit()
+    })
+  })
