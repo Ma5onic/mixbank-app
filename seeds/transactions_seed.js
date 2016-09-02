ab30dd888bc04d391010361e904b711f5def18ab
Madeleine <email": null>
2016-09-02 16:16:23 +1200

    Upgraded transaction seed table and changed login button

--- seeds/transactions_seed.js
+++ seeds/transactions_seed.js
@@ -5,9 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('transactions').insert({id: 1, description: 'bought food at bk', from_account_id: 1, to_account_id: 2, amount: 456}),
-        knex('transactions').insert({id: 2, description: 'weekly wage from bk', from_account_id: 2, to_account_id: 1, amount: 56700}),
-        knex('transactions').insert({id: 3, description: 'miscallaneous', from_account_id: 3, to_account_id: 2, amount: 6000})
+        knex('transactions').insert({id: 1, description: 'Bought a quarter pounder', from_account_id: 1, to_account_id: 2, amount: 456}),
+        knex('transactions').insert({id: 2, description: 'Bought a quarter horse, sweet', from_account_id: 2, to_account_id: 1, amount: 56700}),
+        knex('transactions').insert({id: 3, description: 'Horse feed', from_account_id: 3, to_account_id: 2, amount: 6000}),
+        knex('transactions').insert({id: 4, description: 'Rodeo supplies', from_account_id: 3, to_account_id: 1, amount: 6000}),
+        knex('transactions').insert({id: 5, description: 'Giant wall to stop Mexicans', from_account_id: 2, to_account_id: 1, amount: 900000}),
+        knex('transactions').insert({id: 6, description: 'Hot sauce for James', from_account_id: 3, to_account_id: 1, amount: 255})        
       ]);
     });
 };
